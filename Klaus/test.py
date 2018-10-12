fdea3a99220e64fd9652be1acbf165f90f1c1bc7
Klaus271 <klaus271@outlook.com>
2018-10-11 23:34:00 -0700

    Test1

---
+++ Klaus/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import jieba
+import matplotlib.pyplot as plt
+
+
+data = pd.read_excel("/home/klaus/Documents/sufelaw2019/data/crime1.xlsx")
+print(data)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sufelaw2019
+
+
+This is the private repository for the program.
