d8038f2d75971ccf1f0a6e1bc85d5321a3542013
chenyangzhu <chenyang.zhu@berkeley.edu>
2019-03-31 14:13:04 -0700

    modify codes

--- law/model/classification.py
+++ law/model/classification.py
@@ -1,22 +1,11 @@
-import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from law.model.base_model import Model
 from sklearn.externals import joblib  # 使用sklearn下的工具存储模型
 
 
 class RandomForest(Model):
-    def __init__(self, class_weight=dict({1:0.9, 0:0.1}),
-                       n_estimators=10,
-                       max_depth=10,
-                       min_samples_split=10):
-        '''
-        :param:
-        TODO 补充一下参数的意义
-        class_weight:
-        n_estimators:
-        max_depth:
-        min_samples_splt:
-        '''
+    def __init__(self, class_weight={1: 0.9, 0: 0.1}, n_estimators=10,
+                 max_depth=10, min_samples_split=10):
         super().__init__()
         self.class_weight = class_weight
         self.n_estimators = n_estimators
@@ -28,28 +17,16 @@ class RandomForest(Model):
                                           min_samples_split=self.min_samples_split)
 
     def fit(self, X, y):
-        '''
-        fit 里输入 X 和 y 得到 model
-        :param:
-                X: X
-                y：y
-                *arg: 其他重要的东西继承下来
-        '''
         self.clf.fit(X, y)
 
     def predict(self, new_X):
-        '''
-        这个方程用来predict
-        :param:
-                new_X: 想要predict的X
-        :return:
-                predicted_y
-        '''
         return self.clf.predict(new_X)
 
     def store(self, path="../.cache"):
         joblib.dump(self.clf, path + "/randomforest_model.m")
 
-    def read(self, dir="../.cache/andomforest_model.m"):
-        # 本地调回方法
+
+class Pretrained_RandomForest(RandomForest, Model):
+    def __init__(self, dir="../.cache/andomforest_model.m"):
+        super().__init__()
         self.clf = joblib.load(dir)
