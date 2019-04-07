8f8cc33eb5bfbf1199ceb50a6951a4207cd6a02f
VillardX <43296123+VillardX@users.noreply.github.com>
2019-04-07 11:48:16 +0800

    增加了total_fa_tiao_kuan函数的异常处理功能

--- law/utils.py
+++ law/utils.py
@@ -224,47 +224,61 @@ def ADBinfo(data, index):
 def total_fa_tiao_kuan():
     '''
     output:
-        借本库find_law_tiao_kuan_in_text函数，以dataframe的形式输出数据库中所有案件所涉及的法、条、款的清单
+        借本库find_law_tiao_kuan_in_text函数，以dataframe的形式输出数据库中所有案件所涉及的法、条、款的清单。
+    测试：
+        尝试运行数据库中的civil表，该表共包含26978条数据(截止至2019年4月7日11:45)，执行时间为0:03:29.822323；
+        尝试运行本地数据"侵权.csv"文件，该文件共包含58993条数据，执行时间为0:04:14.327986，尚能尝试改进。
+    备注：
+        另外本法、条、款清单的查重工作尚需完善。
     '''
-
+    
     # 连接数据库
     cnx = pymysql.connect(user='root', password='sufelaw2019',
                                   host='cdb-74dx1ytr.gz.tencentcdb.com',
                                   port = 10008,
                                   database='law')
-
+    
     # 通过pandas阅读数据库内容
     data = pd.read_sql('SELECT * FROM Civil;',con=cnx)
-
+    
+    #记录样本数
+    sample_n = len(data)
+    
     #建立空的法、条、款清单
     df_list = pd.DataFrame(columns = ['fa','tiao','kuan'])
-
+    
+    tiao_pattern = re.compile(r'.*条')
+    
+    
     #填入数据
-    for process in data['process']:
-        x = find_law_tiao_kuan_in_text(process)
-        tiao_pattern = re.compile(r'.*条')
-
-        if len(x) != 0:
-            for element in x:#每个element也是一个列表，每个列表中包含一部法的涉及条款
-                if len(element[1]) == 0 and len(element[2]) == 0:#即这个列表里只有法，没有条也没有款
-                    temp = [element[0],'','']
-                    df_list.loc[df_list.shape[0]] = temp
-                elif len(element[1]) != 0 and len(element[2]) == 0:#即这个列表里有法有条但没有款
-                    for tiao in element[1]:
-                        temp = [element[0],tiao,'']
-                        df_list.loc[df_list.shape[0]] = temp
-                elif len(element[1]) != 0 and len(element[2]) != 0:#即这个列表有法有条有款
-                    existed_tiao_in_kuan = []#存储已包含在element[2]中的条
-                    for tiao_kuan in element[2]:#tiao_kuan是一个字符串
-                        tiao = tiao_pattern.findall(tiao_kuan)#是个list，默认只有一个元素
-                        kuan = tiao_kuan.replace(tiao[0],'')
-                        existed_tiao_in_kuan.append(tiao[0])
-
-                        temp = [element[0],tiao[0],kuan]
-                        df_list.loc[df_list.shape[0]] = temp
-                    existed_tiao_in_kuan = list(set(existed_tiao_in_kuan))#去重
-                    for tiao1 in element[1]:
-                        if tiao1 not in existed_tiao_in_kuan:
-                            temp = [element[0],tiao1,'']
+    for i in range(sample_n):
+        if not pd.isnull(data['process'][i]):#find_law_tiao_kuan_in_text函数不支持nan类型数据输入，故先判断
+            try:
+                x = find_law_tiao_kuan_in_text(data['process'][i])#对于特定的一些字符，find_law_tiao_kuan_in_text会报错
+            except:
+                print("报错：" + data['id'][i])
+            else:  
+                if len(x) != 0:
+                    for element in x:#每个element也是一个列表，每个列表中包含一部法的涉及条款
+                        if len(element[1]) == 0 and len(element[2]) == 0:#即这个列表里只有法，没有条也没有款
+                            temp = [element[0],'','']
                             df_list.loc[df_list.shape[0]] = temp
+                        elif len(element[1]) != 0 and len(element[2]) == 0:#即这个列表里有法有条但没有款
+                            for tiao in element[1]:
+                                temp = [element[0],tiao,'']
+                                df_list.loc[df_list.shape[0]] = temp
+                        elif len(element[1]) != 0 and len(element[2]) != 0:#即这个列表有法有条有款
+                            existed_tiao_in_kuan = []#存储已包含在element[2]中的条
+                            for tiao_kuan in element[2]:#tiao_kuan是一个字符串
+                                tiao = tiao_pattern.findall(tiao_kuan)#是个list，默认只有一个元素
+                                kuan = tiao_kuan.replace(tiao[0],'')
+                                existed_tiao_in_kuan.append(tiao[0])
+
+                                temp = [element[0],tiao[0],kuan]
+                                df_list.loc[df_list.shape[0]] = temp
+                            existed_tiao_in_kuan = list(set(existed_tiao_in_kuan))#去重
+                            for tiao1 in element[1]:
+                                if tiao1 not in existed_tiao_in_kuan:
+                                    temp = [element[0],tiao1,'']
+                                    df_list.loc[df_list.shape[0]] = temp
     return df_list
